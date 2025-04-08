4c9c61de81293140e13b165e84fa5279388fbe18
jfgozzi <jfgozzi@dc.uba.ar>
2025-04-08 14:58:39 -0300

    Update Documento.hs
    invariantes

--- Documento.hs
+++ Documento.hs
@@ -26,12 +26,22 @@ foldDoc fVacio fTexto fLinea doc = case doc of
   where rec = foldDoc fVacio fTexto fLinea 
 
 infixr 6 <+>
-
+--INVARIANTE
+{-
+Como tengo la funcion infixr, que asocia a derecha toda expresion de 3 o mas "<+>", siempre se va a ejecutar con dos "Doc". Ademas, como es un conector
+que se usa sobre las funciones texto, linea y vacio como tal, y no sobre uno de los posibles tipos de dato "Doc", son las propias funciones las que 
+aseguran que se cumplen los invariantes 
+-}
 (<+>) :: Doc -> Doc -> Doc
 d1 <+> d2 = foldDoc d2 (\s acc -> case acc of 
   Texto s2 acc2 -> Texto (s++s2) acc2
   otherwise -> Texto s acc) Linea d1
-
+--INVARIANTE
+{-
+Lo mismo que antes, la funcion se aplica sobre las funciones texto, linea y vacio, que por como son, aseguran que se cumpla en invariante de cada 
+uno de los constructores, luego cada una hace el reemplazo por una estructura propia del data Doc, y con eso el fold identifica cada tipo de estructura 
+del data Doc.
+-}
 indentar :: Int -> Doc -> Doc
 indentar i = foldDoc (Vacio) (\s acc -> Texto s acc) (\i2 acc -> Linea (i+i2) acc)
 
