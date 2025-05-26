08e735f0920ddc3ca053a90a06d0f3d36587e404
jfgozzi <jfgozzi@dc.uba.ar>
2025-05-26 15:38:09 -0300

    Update Main.hs
    agrego test

--- Main.hs
+++ Main.hs
@@ -19,12 +19,20 @@ allTests =
       "Ejercicio 9" ~: testsEj9
     ]
 
+{-
+Respecto a los tests del ejercicio 1, al ser el fold sobre la estructura Doc, no vemos necesario agregar test sobre el, ya que
+todo el resto del trabajo practico fue resuelto en base a esa funcion. Entendemos que los otros ejercicios, como sus respectivos
+test funcionan como propios test de la funcion foldDoc.
+-}
+
 testsEj2 :: Test
 testsEj2 =
   test
     [ vacio <+> vacio ~?= vacio,
       texto "a" <+> texto "b" ~?= texto "ab",
-      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b")
+      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b"),
+      texto "a" <+> texto "b" <+> texto "c" ~?= texto "abc",
+      texto "a" <+> linea <+> texto "b" <+> vacio ~?= texto "a" <+> linea <+> texto "b"
     ]
 
 testsEj3 :: Test
@@ -33,7 +41,9 @@ testsEj3 =
     [ indentar 2 vacio ~?= vacio,
       indentar 2 (texto "a") ~?= texto "a",
       indentar 2 (texto "a" <+> linea <+> texto "b") ~?= texto "a" <+> indentar 2 (linea <+> texto "b"),
-      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a"))
+      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a")),
+      indentar 2 (indentar 2 (linea <+> texto "a")) ~?= indentar 4 (linea <+> texto "a"),
+      indentar 3 (texto "a" <+> indentar 1 (linea <+> texto "b")) ~?= texto "a" <+> indentar 4 (linea <+> texto "b")
     ]
 
 testsEj4 :: Test
@@ -41,8 +51,20 @@ testsEj4 =
   test
     [ mostrar vacio ~?= "",
       mostrar linea ~?= "\n",
-      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b"
+      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b",
+      mostrar (vacio <+> texto "a") ~?= "a",
+      mostrar (indentar 2 (linea <+> indentar 2 (linea <+> texto "a"))) ~?= "\n  \n    a"
     ]
+
+testsEj5 :: Test
+testsEj5 =
+  test
+    [ pponAtomico TextoPP ~?= True,
+      pponAtomico IntPP ~?= True,
+      pponAtomico merlina ~?= False
+    ]
+
+
 
 pericles, merlina, addams, familias :: PPON
 pericles = ObjetoPP [("nombre", TextoPP "Pericles"), ("edad", IntPP 30)]
@@ -54,7 +76,10 @@ testsEj6 :: Test
 testsEj6 =
   test
     [ pponObjetoSimple pericles ~?= True,
-      pponObjetoSimple addams ~?= False
+      pponObjetoSimple addams ~?= False,
+      pponObjetoSimple (ObjetoPP [("x", TextoPP "hola"), ("y", IntPP 42)]) ~?= True,
+      pponObjetoSimple (ObjetoPP [("clave", ObjetoPP [("z", IntPP 3)])]) ~?= False,
+      pponObjetoSimple (ObjetoPP [("clave", TextoPP "a"), ("otra", ObjetoPP [])]) ~?= False
     ]
 
 a, b, c :: Doc
@@ -68,13 +93,19 @@ testsEj7 =
     [ mostrar (intercalar (texto ", ") []) ~?= "",
       mostrar (intercalar (texto ", ") [a, b, c]) ~?= "a, b, c",
       mostrar (entreLlaves []) ~?= "{ }",
-      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}"
+      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}",
+      mostrar (intercalar (texto ", ") [a]) ~?= "a",
+      mostrar (intercalar (texto " + ") [a, b]) ~?= "a + b"
     ]
 
 testsEj8 :: Test
 testsEj8 =
   test
-    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c"
+    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c",
+      mostrar (aplanar (texto "x")) ~?= "x",
+      mostrar (aplanar vacio) ~?= "",
+      mostrar (aplanar (linea <+> texto "a")) ~?= " a",
+      mostrar (aplanar (texto "a" <+> linea)) ~?= "a "
     ]
 
 testsEj9 :: Test
@@ -82,5 +113,8 @@ testsEj9 =
   test
     [ mostrar (pponADoc pericles) ~?= "{ \"nombre\": \"Pericles\", \"edad\": 30 }",
       mostrar (pponADoc addams) ~?= "{\n  \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n  \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n}",
-      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}"
+      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}",
+      mostrar (pponADoc (TextoPP "hola")) ~?= "\"hola\"",
+      mostrar (pponADoc (IntPP 42)) ~?= "42",
+      mostrar (pponADoc (ObjetoPP [])) ~?= "{ }"
     ]
